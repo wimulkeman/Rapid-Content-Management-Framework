4eca0c52a070e40ba074bada7533938387ca6fa1
Wim Ulkeman <wimulkeman@hotmail.com>
2025-05-20 22:41:33 +0200

    style: fix whitespace in router.php to conform to coding standards

--- web/router.php
+++ web/router.php
@@ -14,13 +14,13 @@ declare(strict_types=1);
 // Router for the PHP built-in web server
 if (php_sapi_name() === 'cli-server') {
     // Static file requests
-    if (preg_match('/^\/assets\//', $_SERVER['REQUEST_URI']) || 
+    if (preg_match('/^\/assets\//', $_SERVER['REQUEST_URI']) ||
         preg_match('/\.(jpg|jpeg|png|gif|css|js|ico|svg)$/', $_SERVER['REQUEST_URI'])) {
         return false; // Let the built-in server handle static files
     }
-    
+
     // All other requests go to the front controller
     include __DIR__.'/app.php';
-    
+
     return true;
 }
